70d0b5a146abb3475ab6b7e7e3632df07559082c
RodCato <code@eliment.org>
2023-06-02 17:54:17 -0500

    10 Problems added for JS  practice

---
+++ JS10ProblemSets.js
@@ -0,0 +1,31 @@
+// Question 1:
+// Write a JavaScript function that takes in an array of strings and returns the number of times the letter "a" appears in each string. The function should return an array with the count for each string. For example, given the input ['apple', 'banana', 'orange'], the output should be [1, 3, 1]. What is your solution?
+
+// Question 2:
+// Write a JavaScript function that takes in a string and returns the reversed version of that string. For example, given the input "hello", the output should be "olleh". What is your solution?
+
+// Question 3:
+// Write a JavaScript function that takes in an array of numbers and returns the sum of all the positive numbers in the array. For example, given the input [-1, 2, -3, 4, -5, 6], the output should be 12. What is your solution?
+
+// Question 4:
+// Write a JavaScript function that takes in a string and checks if it is a palindrome. A palindrome is a word, phrase, number, or other sequence of characters that reads the same forward and backward, ignoring spaces, punctuation, and capitalization. The function should return true if the string is a palindrome, and false otherwise. For example, given the input "racecar", the output should be true. What is your solution?
+
+// Question 5:
+// Write a JavaScript function that takes in an array of numbers and returns the product of all the even numbers in the array. For example, given the input [1, 2, 3, 4, 5, 6], the output should be 48. What is your solution?
+
+// Question 6:
+// Write a JavaScript function that takes in an array of numbers and returns a new array containing only the even numbers. For example, given the input [1, 2, 3, 4, 5, 6], the output should be [2, 4, 6]. What is your solution?
+
+// Question 7:
+// Write a JavaScript function that takes in a string and returns the number of vowels (a, e, i, o, u) in the string. The function should be case-insensitive, meaning it should count both uppercase and lowercase vowels. For example, given the input "Hello World", the output should be 3. What is your solution?
+
+// Question 8:
+// Write a JavaScript function that takes in an array of strings and returns the shortest string in the array. If there are multiple shortest strings, it should return the one that appears first. For example, given the input ["apple", "banana", "pear", "kiwi"], the output should be "pear". What is your solution?
+
+// Question 9:
+// Write a JavaScript function that takes in a number and checks if it is a prime number. A prime number is a number greater than 1 that is only divisible by 1 and itself. The function should return true if the number is prime, and false otherwise. For example, given the input 7, the output should be true. What is your solution?
+
+// Question 10:
+// Write a JavaScript function that takes in a sorted array of numbers and finds the missing number in the sequence. The array should contain a sequence of consecutive numbers, but one number is missing. The function should return the missing number. For example, given the input [1, 2, 3, 5, 6, 7], the output should be 4. What is your solution?
+
+
